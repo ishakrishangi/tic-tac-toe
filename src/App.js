588ec2940db5c1a30592ed24409d99a60d38fc09
ishakrishangi <krishangik@gmail.com>
2022-08-19 13:30:02 -0400

    pushing part 4 i think

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React, {useState} from 'react';
-import './App.css';
-import Square from './Square';
+import React, { useState } from "react";
+import "./App.css";
+import Square from "./Square";
 
 function App() {
-  const [squares, setSquares] = useState(["","","","","","","","",""]);
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
   // const propVariable = 'This is a prop'
 
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""])
+    setPlayer(true)
+  }
+
   return (
     <div className="App">
-      <Square 
-      // propsVar={propVariable}
-      squares={squares}
-      setSquares={setSquares}
-      player={player}
-      setPlayer={setPlayer}
-
-      />
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+              squareValue={value}
+              index={index}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
   );
 }
